a784b20ca80ffe21465509e68b3b08d1d1ffd1c7
wickieonya <wickieonya@gmail.com>
2019-11-02 12:32:23 +0300

    added spinner and keys

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false,
-  }
+    loading: false
+  };
 
   componentDidMount() {
     this.setState({ loading: true });
-    axios.get('https://api.github.com/users')
-    .then(response => {
-      this.setState({users: response.data, loading: false})
-    })
-    .catch(error => console.log(error));
-  } 
+    const url = `https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`;
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({ users: response.data, loading: false });
+      })
+      .catch(error => console.log(error));
+  }
   render() {
     return (
-    <div className="App">
-      <Navbar title="Github Finder" />
-      <div className="container">
-        <Users 
-          users={this.state.users}
-          loading={this.state.loading}
-        />
+      <div className="App">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
       </div>
-    </div>       
     );
   }
 }
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
-const Users = ({ users, loading }) =>
-    <div style={userStyle}>
-        {users.map(user => (
-            <UserItem
-                key={user.id}
-                login={user.login}
-                avatar_url={user.avatar_url}
-                html_url={user.html_url}
-            />
-        ))}
-    </div>
+const Users = ({ users, loading }) => {
+    if(loading){
+        return <Spinner></Spinner>
+    } else {
+        return (
+            <div style={userStyle}>
+            {users.map(user => (
+                <UserItem
+                    key={user.id}
+                    login={user.login}
+                    avatar_url={user.avatar_url}
+                    html_url={user.html_url}
+                />
+            ))}
+        </div> 
+        )
+    }
+}
 
 const userStyle = {
     display: 'grid',
